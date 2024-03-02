8b02a7dbf9c2b496b73f7bab29e52fb6a7f08e50
SkyMasterROG <modder1980@gmail.com>
2024-03-02 16:03:17 +0100

    update GenericContainerScreenMixin

--- src/main/java/net/wurstclient/mixin/GenericContainerScreenMixin.java
+++ src/main/java/net/wurstclient/mixin/GenericContainerScreenMixin.java
@@ -55,11 +55,11 @@ public abstract class GenericContainerScreenMixin
 		{
 			addDrawableChild(ButtonWidget
 				.builder(Text.literal("Steal"), b -> steal())
-				.dimensions(x + backgroundWidth - 108, y + 4, 50, 12).build());
+				.dimensions(x + backgroundWidth - 108, y - 12, 50, 12).build());
 			
 			addDrawableChild(ButtonWidget
 				.builder(Text.literal("Store"), b -> store())
-				.dimensions(x + backgroundWidth - 56, y + 4, 50, 12).build());
+				.dimensions(x + backgroundWidth - 56, y - 12, 50, 12).build());
 		}
 		
 		if(autoSteal.isEnabled())
